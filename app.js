67eab70836313b74472dcf0cc1561a831bd57abd
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-06-11 00:58:14 +0700

    Express

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express   = require('express');
 const app       = express();
+const path 		= require('path');
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/index', (req,res) =>{
     res.json({
         msg : 'Index page'
     });
 });
 
+app.use(express.static('./'))
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 });
 app.listen('8080', ()=>{
