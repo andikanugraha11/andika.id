78adc3646c15ecdb84382d3a3f0a77ed2c6a1cc0
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-06-12 13:45:37 +0700

    Tracker

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
 const express   = require('express');
 const app       = express();
 const path 		= require('path');
+const os 		= require('os');
+const mongoose	= require('mongoose');
 
+const Visitor = require('./models/visitorSchema');
+
+var url = 'mongodb://andika:andika123@ds255740.mlab.com:55740/heroku_tt0g602l';
+
+
+mongoose.connect(url);
+
+//on connect Database
+mongoose.connection.on('connected', () => {
+    console.log(`database terhubung dengan ${url}`);
+});
+
+//on Error database
+mongoose.connection.on('error', (err) => {
+    console.log(`Error : ${err}`);
+});
+				
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/index', (req,res) =>{
+    const visitor = {
+        visitor: os.hostname()
+    }
+    console.log('hehe')
+    console.log(visitor)
     res.json({
         msg : 'Index page'
     });
 });
 
 const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    
+    const visitor = {
+        visitor: os.hostname()
+    }
+    console.log('hehe')
+    console.log(visitor)
+    const newVisit = new Visitor(visitor);
+    newVisit.save(function (err) {
+        if (!err){
+            res.sendFile(path.join(__dirname, 'public/index.html'))
+        }else{
+            console.log(err)
+        }
+        
+    })
+    
+});
 
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-});
+
 app.listen(port, ()=>{
-    console.log('express run !!');
+    console.log(`express run !! ${port}`);
 });
